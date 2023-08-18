2a29321edee13c158c871466a7a2e18d3524a06c
Xinyuuu7 <124234708+Xinyuuu7@users.noreply.github.com>
2023-08-18 12:00:28 +0800

    Add files via upload

---
+++ src/main/java/BastionGenerator/reecriture/BastionPools/TreasureJigsawBlocks.java
@@ -0,0 +1,17 @@
+package BastionGenerator.reecriture.BastionPools;
+
+import BastionGenerator.enumType.PoolType;
+import com.seedfinding.mccore.block.Block;
+import com.seedfinding.mccore.util.block.BlockDirection;
+import com.seedfinding.mccore.util.data.Pair;
+import com.seedfinding.mccore.util.data.Quad;
+import com.seedfinding.mccore.util.pos.BPos;
+
+import java.util.HashMap;
+import java.util.List;
+
+public class TreasureJigsawBlocks {
+    public static final HashMap<String, List<Pair<Quad<PoolType, String, Pair<BlockDirection, BlockDirection>, Block>, BPos>>> JIGSAW_BLOCKS = new HashMap<>() {{
+        this.putAll(MobsJigsawBlocks.JIGSAW_BLOCKS);
+    }};
+}
