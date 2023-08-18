84da70bab373c50d303d57f787d077bed90474f4
De6ris <2838369930@qq.com>
2023-08-18 20:27:15 +0800

    tiny things for test

--- src/main/java/BastionGenerator/properties/BastionGenerator.java
+++ src/main/java/BastionGenerator/properties/BastionGenerator.java
@@ -1,8 +1,17 @@
 package BastionGenerator.properties;
 
+import com.seedfinding.mccore.util.pos.BPos;
+
+import java.util.List;
+
 public class BastionGenerator {
+    public List<Piece> pieces;
     public enum PlacementBehaviour {
         RIGID,
         TERRAIN_MATCHING,
+    }
+    static public class Piece {
+        String name;
+        public BPos pos;
     }
 }
