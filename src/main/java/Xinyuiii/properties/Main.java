473166f7a6c7c06c5e3498f6ff125edb8b9db0c3
Xinyuuu7 <124234708+Xinyuuu7@users.noreply.github.com>
2023-09-01 16:36:11 +0800

    Add files via upload

---
+++ src/main/java/Xinyuiii/properties/Main.java
@@ -0,0 +1,39 @@
+package Xinyuiii.properties;
+
+import com.seedfinding.mccore.rand.ChunkRand;
+import com.seedfinding.mccore.rand.seed.WorldSeed;
+import com.seedfinding.mccore.util.data.Pair;
+import com.seedfinding.mccore.util.pos.BPos;
+import com.seedfinding.mccore.util.pos.CPos;
+import com.seedfinding.mccore.version.MCVersion;
+import com.seedfinding.mcfeature.loot.item.ItemStack;
+import com.seedfinding.mcfeature.structure.BastionRemnant;
+
+import java.util.List;
+import java.util.Random;
+
+public class Main {
+    public static void main(String[] args) {
+        Random random = new Random();
+        ChunkRand rand = new ChunkRand();
+        BastionRemnant bastionRemnant = new BastionRemnant(MCVersion.v1_20);
+        while (true) {
+            long seed = random.nextLong();
+            long structureSeed = WorldSeed.toStructureSeed(seed);
+            CPos pos = bastionRemnant.getInRegion(structureSeed,0,0,rand);
+            if (pos == null) {
+                continue;
+            }
+            BastionGenerator generator = new BastionGenerator(MCVersion.v1_20);
+            generator.generate(seed,pos);
+            System.out.println(seed);
+            System.out.println(pos.toBlockPos());
+            for (Pair<BPos, List<ItemStack>> chest : generator.generateLoot()) {
+                System.out.println(chest.getFirst());
+                for (ItemStack item : chest.getSecond()) {
+                    System.out.println(item.getCount() + " " + item.getItem().getEnchantments() + " " + item.getItem().getName());
+                }
+            }return;
+        }
+    }
+}
