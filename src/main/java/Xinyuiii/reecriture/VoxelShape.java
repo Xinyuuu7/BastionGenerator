7f8abb076bc75cd086a85d4e6f46172239d1eb76
Xinyuuu7 <124234708+Xinyuuu7@users.noreply.github.com>
2024-02-14 14:56:26 +0800

    Update VoxelShape.java

--- src/main/java/Xinyuiii/reecriture/VoxelShape.java
+++ src/main/java/Xinyuiii/reecriture/VoxelShape.java
@@ -17,38 +17,44 @@ public class VoxelShape {
         this.ys = null;
         this.zs = null;
     }
+
     public VoxelShape(BlockBox box) {
         xs = Arrays.asList((double)box.minX,(double)box.maxX);
         ys = Arrays.asList((double)box.minY,(double)box.maxY);
         zs = Arrays.asList((double)box.minZ,(double)box.maxZ);
-
     }
 
     public boolean isNull(){
         return (this.xs == null);
     }
+
     public void setValue(BlockBox box, boolean b) {
         this.xs = Arrays.asList((double)box.minX,(double)box.maxX+1.0);
         ys = Arrays.asList((double)box.minY,(double)box.maxY+1.0);
         zs = Arrays.asList((double)box.minZ,(double)box.maxZ+1);
     }
+
     public List<Double> getX(){
         return this.xs;
     }
-    public double getLastX(){
-        return this.xs.get(xs.size()-1);
-    }
-    public double getLastY(){
-        return this.ys.get(ys.size()-1);
-    }
-    public double getLastZ(){
-        return this.zs.get(zs.size()-1);
 
-    }
     public List<Double> getY(){
         return this.ys;
     }
+
     public List<Double> getZ(){
         return this.zs;
+    }
+
+    public double getLastX(){
+        return this.xs.get(xs.size()-1);
+    }
+
+    public double getLastY(){
+        return this.ys.get(ys.size()-1);
+    }
+
+    public double getLastZ(){
+        return this.zs.get(zs.size()-1);
     }
 }
